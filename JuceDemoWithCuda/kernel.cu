1b837bc3ecb8d08834e6469b860a24e4c7803818
Colin Wallace <wallacoloo@gmail.com>
2015-02-02 18:27:20 -0800

    Reuse a large circular buffer on the device side that will make the delay effect possible.

--- JuceDemoWithCuda/kernel.cu
+++ JuceDemoWithCuda/kernel.cu
@@ -9,9 +9,15 @@
 
 #include "defines.h"
 
+#define CIRCULAR_BUFFER_LEN MAX_DELAY_EFFECT_LENGTH
+
 // When summing the outputs at a specific frame for each partial, we use a reduction method.
 // This reduction method requires a temporary array in shared memory.
 __shared__ float partialReductionOutputs[NUM_PARTIALS*NUM_CH];
+
+// this is a circular buffer of sample data (interleaved by channel number) stored on the device
+// It is persistent and lengthy, in order to accomodate the delay effect.
+float *d_sampleBuffer = NULL;
 
 void checkCudaError(cudaError_t e) {
 	if (e != cudaSuccess) {
@@ -20,18 +26,6 @@ void checkCudaError(cudaError_t e) {
 		exit(1);
 	}
 }
-
-// code to run at shutdown (free buffers, etc)
-void teardown() {
-
-}
-
-// code to run on first-time audio calculation
-void startup() {
-	atexit(&teardown);
-
-}
-
 
 bool _hasCudaDevice() {
 	int deviceCount;
@@ -48,6 +42,41 @@ bool hasCudaDevice() {
 	return hasDevice;
 }
 
+// code to run at shutdown (free buffers, etc)
+void teardown() {
+	// free the sample buffer if we allocated it and it hasn't already been freed.
+	if (d_sampleBuffer != NULL) {
+		if (hasCudaDevice()) {
+			checkCudaError(cudaFree(d_sampleBuffer));
+		} else {
+			free(d_sampleBuffer);
+		}
+		// avoid double-frees
+		d_sampleBuffer = NULL;
+	}
+}
+
+// code to run on first-time audio calculation
+void startup() {
+	atexit(&teardown);
+	if (hasCudaDevice()) {
+		// allocate sample buffer on device
+		checkCudaError(cudaMalloc(&d_sampleBuffer, CIRCULAR_BUFFER_LEN*NUM_CH*sizeof(float)));
+	} else {
+		// allocate sample buffer on cpu
+		d_sampleBuffer = (float*)malloc(CIRCULAR_BUFFER_LEN*NUM_CH*sizeof(float));
+	}
+}
+
+void doStartupOnce() {
+	static bool hasInit = false;
+	if (!hasInit) {
+		startup();
+		hasInit = true;
+	}
+}
+
+
 __device__ __host__ void reduceOutputs(float *buffer, unsigned partialIdx, int sampleIdx, float outputL, float outputR) {
 	//algorithm: given 8 outputs, [0, 1, 2, 3, 4, 5, 6, 7]
 	//first iteration: 4 active threads. 
@@ -60,30 +89,32 @@ __device__ __host__ void reduceOutputs(float *buffer, unsigned partialIdx, int s
 	//  Thread 0 adds i0 to i(0+1).
 	//  Output now: [28,   16, 8, 10, 4, 5, 6, 7]
 	//fourth iteration: 0 active threads -> exit
+	unsigned bufferIdx = NUM_CH * (sampleIdx % CIRCULAR_BUFFER_LEN);
 	#ifdef __CUDA_ARCH__
 		//device code
-		partialReductionOutputs[partialIdx * 2 + 0] = outputL;
-		partialReductionOutputs[partialIdx * 2 + 1] = outputR;
+		partialReductionOutputs[NUM_CH*partialIdx + 0] = outputL;
+		partialReductionOutputs[NUM_CH*partialIdx + 1] = outputR;
 		unsigned numActiveThreads = NUM_PARTIALS / 2;
 		while (numActiveThreads > 0) {
 			__syncthreads();
-			partialReductionOutputs[partialIdx] += partialReductionOutputs[partialIdx + numActiveThreads];
+			partialReductionOutputs[NUM_CH*partialIdx + 0] += partialReductionOutputs[NUM_CH*partialIdx + numActiveThreads*NUM_CH + 0];
+			partialReductionOutputs[NUM_CH*partialIdx + 1] += partialReductionOutputs[NUM_CH*partialIdx + numActiveThreads*NUM_CH + 1];
 			numActiveThreads /= 2;
 		}
 		if (partialIdx == 0) {
-			buffer[sampleIdx * 2 + 0] = partialReductionOutputs[0];
-			buffer[sampleIdx * 2 + 1] = partialReductionOutputs[1];
+			buffer[bufferIdx + 0] = partialReductionOutputs[0];
+			buffer[bufferIdx + 1] = partialReductionOutputs[1];
 		}
 	#else
 		//host code
 		//Since everything's computed iteratively, we can just add our outputs directly to the buffer.
 		//First write to this sample must zero-initialize the buffer (not required in the GPU code).
 		if (partialIdx == 0) {
-			buffer[sampleIdx * 2 + 0] = 0;
-			buffer[sampleIdx * 2 + 1] = 0;
+			buffer[bufferIdx + 0] = 0;
+			buffer[bufferIdx + 1] = 0;
 		}
-		buffer[sampleIdx * 2 + 0] += outputL;
-		buffer[sampleIdx * 2 + 1] += outputR;
+		buffer[bufferIdx + 0] += outputL;
+		buffer[bufferIdx + 1] += outputR;
 	#endif
 }
 
@@ -93,7 +124,7 @@ __device__ __host__ void computePartialOutput(float *buffer, unsigned baseIdx, u
 	for (int sampleIdx = 0; sampleIdx < BUFFER_BLOCK_SIZE; ++sampleIdx) {
 		float outputL, outputR;
 		outputL = outputR = (1.0/NUM_PARTIALS)*sinf((baseIdx + sampleIdx) * angleDelta);
-		reduceOutputs(buffer, partialIdx, sampleIdx, outputL, outputR);
+		reduceOutputs(buffer, partialIdx, baseIdx+sampleIdx, outputL, outputR);
 	}
 }
 
@@ -102,28 +133,28 @@ __global__ void fillSineWaveKernel(float *buffer, unsigned baseIdx, float fundam
 	computePartialOutput(buffer, baseIdx, partialNum, fundamentalFreq);
 }
 
-__host__ void fillSineWaveOnCpu(float *buffer, unsigned baseIdx, float fundamentalFreq) {
+__host__ void fillSineWaveOnCpu(float *bufferB, unsigned sampleIdx, float fundamentalFreq) {
 	for (int partialIdx = 0; partialIdx < NUM_PARTIALS; ++partialIdx) {
-		computePartialOutput(buffer, baseIdx, partialIdx, fundamentalFreq);
+		computePartialOutput(d_sampleBuffer, sampleIdx, partialIdx, fundamentalFreq);
 	}
+	unsigned bufferStartIdx = NUM_CH * (sampleIdx % CIRCULAR_BUFFER_LEN);
+	memcpy(bufferB, &d_sampleBuffer[bufferStartIdx], BUFFER_BLOCK_SIZE*NUM_CH*sizeof(float));
 }
 
-__host__ void fillSineWaveCuda(float *bufferB, unsigned baseIdx, float fundamentalFreq) {
-	float *gpuOutBuff;
-	//allocate buffer memory. No need for it to be zero'd
-	checkCudaError(cudaMalloc(&gpuOutBuff,    BUFFER_BLOCK_SIZE*NUM_CH*sizeof(float)));
-	fillSineWaveKernel << <1, NUM_PARTIALS >> >(gpuOutBuff, baseIdx, fundamentalFreq);
-	checkCudaError(cudaGetLastError()); //check if error in kernel launch
+__host__ void fillSineWaveCuda(float *bufferB, unsigned sampleIdx, float fundamentalFreq) {
+	fillSineWaveKernel << <1, NUM_PARTIALS >> >(d_sampleBuffer, sampleIdx, fundamentalFreq);
 
+	checkCudaError(cudaGetLastError()); //check if error in kernel launch
 	checkCudaError(cudaDeviceSynchronize()); //check for error INSIDE the kernel
 
 	//copy memory into the cpu buffer
 	//Note: this will wait for the kernel to complete first.
-	checkCudaError(cudaMemcpy(bufferB, gpuOutBuff, BUFFER_BLOCK_SIZE*NUM_CH*sizeof(float), cudaMemcpyDeviceToHost));
-	checkCudaError(cudaFree(gpuOutBuff));
+	unsigned bufferStartIdx = NUM_CH * (sampleIdx % CIRCULAR_BUFFER_LEN);
+	checkCudaError(cudaMemcpy(bufferB, &d_sampleBuffer[bufferStartIdx], BUFFER_BLOCK_SIZE*NUM_CH*sizeof(float), cudaMemcpyDeviceToHost));
 }
 
 void fillSineWaveVoice(float *bufferB, unsigned baseIdx, float fundamentalFreq) {
+	doStartupOnce();
 	if (hasCudaDevice()) {
 		fillSineWaveCuda(bufferB, baseIdx, fundamentalFreq);
 	} else {
